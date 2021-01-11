a4b3ca4e4c2e9adf12eaa86a28e3ae1b3f93e832
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-01-11 20:27:01 +0530

    altered colors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const withSass = require('@zeit/next-sass')
-const path = require('path');
+const path = require('path')
 
-module.exports = withSass({
-    cssModules: true,
+module.exports = {
     sassOptions: {
-        // Resolving SASS absolute imports
-        includePaths: [path.resolve(__dirname, 'styles')],
+        includePaths: [path.join(__dirname, 'styles')],
     },
-    cssLoaderOptions: {
-        localIdentName: '[hash:base64]',
-    },
-})
+}
