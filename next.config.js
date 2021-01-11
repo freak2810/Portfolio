af95516fb50384b95c2a31920b080ede8f16c55a
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-01-11 16:25:37 +0530

    added sass config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,9 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-
-const debug = process.env.NODE_ENV !== "production";
+const path = require('path');
 
 module.exports = {
-    exportPathMap: function () {
-        return {
-            "/": { page: "/" },
-        }
+    basePath: '/',
+    assetPrefix: '/',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
     },
-    //assetPrefix: '',
-    assetPrefix: !debug ? '/portfolio/' : '',
-    webpack: (config, { dev }) => {
-        // Perform customizations to webpack config
-        // console.log('webpack');
-        // console.log(config.module.rules, dev);
-        config.module.rules = config.module.rules.map(rule => {
-            if(rule.loader === 'babel-loader') {
-                rule.options.cacheDirectory = false
-            }
-            return rule
-        })
-        // Important: return the modified config
-        return config
-    }/*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
 }
