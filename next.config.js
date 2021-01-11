1c64560875ffc5bfa8dff5b217e3d12eede4ac47
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-01-11 17:01:32 +0530

    updated sass config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-    cssModules: true,
-    cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]___[hash:base64:5]",
-    }
-})
+const path = require('path')
+
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+
+    },
+}
