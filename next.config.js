6a11143c75437c4ebf53cb8db29430c016e2c220
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2021-02-04 12:10:20 +0530

    addd PWA support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-const path = require('path')
-module.exports = {
+const path = require('path');
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = {
+//     sassOptions: {
+//         includePaths: [path.join(__dirname, 'styles')],
+//     },
+// }
+
+module.exports = withPWA({
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
-}
+    pwa: {
+        dest: 'public',
+        runtimeCaching,
+    },
+})
