75eb24fe47ca79c5cbff0e757f273a0a6af5ea3f
freak2810 <adityamanikanthrao@gmail.com>
2021-08-21 09:54:38 +0530

    added cover for projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import client from '../apollo/client';
 import GET_PROJECT_PAGE_PROPS from '../apollo/queries/getProjectsPageProps';
 import SEO from '../components/SEO';
@@ -13,61 +14,74 @@ export default function Projects({ projects }: ProjectPageProps) {
 			<SEO title={`Aditya's Portfolio - Projects`} />
 			<section className='container px-4 mx-auto mt-20 lg:max-w-7xl'>
 				<h1 className='mb-4 text-5xl font-semibold text-center md:text-6xl'>
-					Technologies
+					Projects
 				</h1>
 				<main>
 					{projects.map(p => (
-						<article
-							key={p.id}
-							className='my-4 md:my-8 p-2 pl-4 transition-all duration-300 border-2 border-l-2 rounded-lg hover:shadow-xl border-[transparent] hover:border-text shadow-[#00ff00]'>
-							<a
-								href={p.githubLink}
-								target='blank'
-								className='text-2xl font-medium transition-all duration-300 hover:text-splash'>
-								{p.name}
-							</a>
-							<div className='flex flex-wrap my-2'>
-								{p.technologies.map(t => (
-									<p
-										key={t.name}
-										className='px-2 mb-2 mr-2 transition-shadow duration-300 border cursor-pointer rounded-xl hover:shadow-inner'>
-										{t.name}
-									</p>
-								))}
-							</div>
-							{p.description && (
-								<ReactMarkdown className='md'>
-									{p.description.markdown}
-								</ReactMarkdown>
-							)}
-							<div className='flex my-2 '>
-								{p.githubLink && (
-									<a
-										href={p.githubLink}
-										target='blank'
-										title='Checkout the code'>
-										<Code
-											className='mr-4 transition-all duration-300 hover:text-splash'
-											height={32}
-											width={32}
+						<a target='blank' key={p.id} href={p.githubLink || p.deploymentUrl}>
+							<article className='my-4 md:my-8 p-2 pl-4 transition-all duration-300 border-2 border-l-2 rounded-lg hover:shadow-xl border-[transparent] hover:border-text shadow-[#00ff00]'>
+								<div className='my-1'>
+									{p.cover && (
+										<Image
+											layout='responsive'
+											objectFit='cover'
+											objectPosition='center top'
+											height={1}
+											width={5}
+											src={p.cover?.url}
+											alt={p.name}
 										/>
-									</a>
+									)}
+								</div>
+								<a
+									href={p.githubLink}
+									target='blank'
+									className='text-2xl font-medium transition-all duration-300 hover:text-splash'>
+									{p.name}
+								</a>
+								<div className='flex flex-wrap my-2'>
+									{p.technologies.map(t => (
+										<p
+											key={t.name}
+											className='px-2 mb-2 mr-2 transition-shadow duration-300 border cursor-pointer rounded-xl hover:shadow-inner'>
+											{t.name}
+										</p>
+									))}
+								</div>
+								{p.description && (
+									<ReactMarkdown className='md'>
+										{p.description.markdown}
+									</ReactMarkdown>
 								)}
+								<div className='flex my-2 '>
+									{p.githubLink && (
+										<a
+											href={p.githubLink}
+											target='blank'
+											title='Checkout the code'>
+											<Code
+												className='mr-4 transition-all duration-300 hover:text-splash'
+												height={32}
+												width={32}
+											/>
+										</a>
+									)}
 
-								{p.deploymentUrl && (
-									<a
-										href={p.deploymentUrl}
-										target='blank'
-										title='Checkout the Deployment'>
-										<Web
-											className='transition-all duration-300 hover:text-splash'
-											height={32}
-											width={32}
-										/>
-									</a>
-								)}
-							</div>
-						</article>
+									{p.deploymentUrl && (
+										<a
+											href={p.deploymentUrl}
+											target='blank'
+											title='Checkout the Deployment'>
+											<Web
+												className='transition-all duration-300 hover:text-splash'
+												height={32}
+												width={32}
+											/>
+										</a>
+									)}
+								</div>
+							</article>
+						</a>
 					))}
 				</main>
 			</section>
