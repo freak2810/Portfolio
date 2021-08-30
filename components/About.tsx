621713e560bb69537a508fb8be491a87d225c78d
freak2810 <adityamanikanthrao@gmail.com>
2021-08-30 20:24:37 +0530

    added some additional styles for markdown in about

--- components/About.tsx
+++ components/About.tsx
@@ -4,10 +4,12 @@ import { AboutProps } from '../types/LandingPageProps';
 export default function About({ about, image }: AboutProps) {
 	return (
 		<div className=''>
-			<h1 className='text-5xl text-center'>About</h1>
+			<h1 className='mb-4 text-5xl font-semibold text-center md:text-6xl'>
+				About
+			</h1>
 			<div className='flex flex-col-reverse items-center justify-center px-5 my-10 lg:px-10 lg:flex-row'>
-				<div className='px-4 my-10 lg:px-8 lg:w-2/3 lg:my-0'>
-					<ReactMarkdown className='md about'>{about}</ReactMarkdown>
+				<div className='px-2 my-10 lg:px-8 lg:w-2/3 lg:my-0'>
+					<ReactMarkdown className='lg:text-lg md about'>{about}</ReactMarkdown>
 				</div>
 				<img
 					src={image}
