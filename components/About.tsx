933cb76c4921ecde7061566a04bde6f7feb5f5c9
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-11-01 23:57:16 +0530

    clean up

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,5 @@
-import ReactMarkdown from 'react-markdown';
 import { AboutProps } from '../types/LandingPageProps';
-// @ts-expect-error - No types available
-import toMarkdown from '@sanity/block-content-to-markdown';
-// import Image from 'next/legacy/image';
+import { PortableText } from '@portabletext/react';
 import Image from 'next/image';
 
 export default function About({ about, image }: AboutProps) {
@@ -13,10 +10,9 @@ export default function About({ about, image }: AboutProps) {
 			</h1>
 			<div className='flex flex-col-reverse items-center justify-center px-5 my-10 lg:px-10 lg:flex-row'>
 				<div className='px-2 my-10 lg:px-8 lg:w-2/3 lg:my-0'>
-					{/* @ts-ignore */}
-					<ReactMarkdown className='text-lg md about'>
-						{toMarkdown(about)}
-					</ReactMarkdown>
+					<div className='text-lg md about'>
+						<PortableText value={about} />
+					</div>
 				</div>
 				<Image
 					className='cursor-pointer rounded-xl'
