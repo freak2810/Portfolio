8f548f0a098eded3ad9dc89c1ac504c449af3b30
Aditya Manikanth Rao <adityamanikanthrao@gmail.com>
2023-11-02 23:09:41 +0530

    updates

--- components/TypeWriterEffect.tsx
+++ components/TypeWriterEffect.tsx
@@ -1,27 +1,14 @@
-'use client';
-
 import { Maybe } from 'graphql/jsutils/Maybe';
 import styles from '../styles/type-writer.module.scss';
-import { useEffect, useState } from 'react';
 
 interface Roles {
 	roles: Maybe<Maybe<string>[]> | undefined;
 }
 
 export default function TypeWriterEffect({ roles }: Roles) {
-	const [currentRole, switchCurrentRole] = useState(0);
-
-	useEffect(() => {
-		console.log('use effect');
-		switchCurrentRole((currentRole + 1) % roles?.length!);
-	}, []);
-
 	return (
 		<div className={styles.typeWriter}>
-			<span className={styles.firstText}>I am a </span>
-			<span className={styles.secondText}>
-				{roles ? roles[currentRole] : ''}
-			</span>
+			<span className={styles.secondText}>{roles ? roles[0] : ''}</span>
 		</div>
 	);
 }
